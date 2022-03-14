34862fe68c68822a973b5ba07944989bc6a84b79
Gaoming Lin <gaominglins@gmail.com>
2022-03-14 00:40:54 -0400

    p

---
+++ assignment/hook_form/src/components/UserForm.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from  'react';
+    
+    
+const UserForm = (props) => {
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");  
+    
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = { firstname, lastname, email, password };
+        console.log("Welcome", newUser);
+    };
+    
+    return(
+        <form onSubmit={ createUser }>
+            <div>
+                <label>firstname: </label> 
+                <input type="text" onChange={ (e) => setFirstname(e.target.value) } />
+            </div>
+            <div>
+                <label>lastname: </label> 
+                <input type="text" onChange={ (e) => setLastname(e.target.value) } />
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+            </div>
+            <input type="submit" value="Create User" />
+        </form>
+    );
+};
+    
+export default UserForm;
