2776bf6c375987b51adcebce13c912e50fe906db
Gaoming Lin <gaominglins@gmail.com>
2022-03-14 00:55:43 -0400

    p

--- assignment/hook_form/src/components/UserForm.jsx
+++ assignment/hook_form/src/components/UserForm.jsx
@@ -6,15 +6,17 @@ const UserForm = (props) => {
     const [lastname, setLastname] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");  
+    const [comfirm_password, setComfirm_Password] = useState("");  
     
     const createUser = (e) => {
         e.preventDefault();
-        const newUser = { firstname, lastname, email, password };
+        const newUser = { firstname, lastname, email, password, comfirm_password };
         console.log("Welcome", newUser);
     };
     
     return(
-        <form onSubmit={ createUser }>
+        <>
+            <form onSubmit={ createUser }>
             <div>
                 <label>firstname: </label> 
                 <input type="text" onChange={ (e) => setFirstname(e.target.value) } />
@@ -31,8 +33,20 @@ const UserForm = (props) => {
                 <label>Password: </label>
                 <input type="text" onChange={ (e) => setPassword(e.target.value) } />
             </div>
+            <div>
+                <label>Comfirm Password: </label>
+                <input type="text" onChange={ (e) => setComfirm_Password(e.target.value) } />
+            </div>
             <input type="submit" value="Create User" />
-        </form>
+            </form>
+
+            <p>First Name: {firstname}</p>
+            <p>Last Name: {lastname}</p>
+            <p>Email: {email}</p>
+            <p>Password: {password}</p>
+            <p>Comfirm Password: {comfirm_password}</p>
+        </>
+        
     );
 };
     
