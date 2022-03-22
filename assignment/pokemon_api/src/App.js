056c767d675b2a848aad388dbf2a21065f5daad6
Gaoming Lin <gaominglins@gmail.com>
2022-03-22 15:32:27 -0400

    p

--- assignment/pokemon_api/src/App.js
+++ assignment/pokemon_api/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <h1>APIs Assignments</h1>
       <PokemonAPI></PokemonAPI>
-      <AxiosPokemonAPI> </AxiosPokemonAPI>
+      {/* <AxiosPokemonAPI> </AxiosPokemonAPI> */}
 
 
     </div>
