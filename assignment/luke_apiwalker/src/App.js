eccf21138fe22d2432e904ede280785f962b49c1
Gaoming Lin <gaominglins@gmail.com>
2022-03-26 00:39:25 -0400

    p

--- assignment/luke_apiwalker/src/App.js
+++ assignment/luke_apiwalker/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter, //this allows us to enable routing for the application
+  Switch, //this allows us to tell react which components/elements will show conditionally based on the route
+  Route, //this allows us to specify the route for a component/element
+  Link //just like an anchor tag but it will not reload the whole page
+} from "react-router-dom";
+
+import Profile from './components/Profile';
+import People from './components/People';
+import PeopleSearchForm from './components/PeopleSearchForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Welcome to Routing Demo</h1>
+        <Link to="/">Home</Link> || <Link to="/profile">View My Profile</Link>
+        <PeopleSearchForm></PeopleSearchForm>
+        <Switch>
+          <Route exact path = "/">
+            <p>This is the home page</p>
+          </Route>
+          <Route exact path = "/profile">
+            <Profile></Profile>
+          </Route>
+          <Route exact path = "/people/:id/">
+            <People></People>
+          </Route>
+
+          <Route exact path = "/people/:id/:borderColor">
+            <People></People>
+          </Route>
+          
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
